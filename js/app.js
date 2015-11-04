4455645f9a6a1892787e27503795b46c27131dfe
Sage Elliott <wildtroll@gmail.com>
2015-11-03 21:36:58 -0800

    Add death counter:
    - add disabled input form to index.html
    - target and update form in player update fucntion in app.js
    - style in style.css

--- js/app.js
+++ js/app.js
@@ -105,21 +105,24 @@ Player.prototype.update = function(){
             this.x-= 100
          }
 
-    //Update Score Value
-    document.getElementById('score').value = score;
-
     // Detect collision with enemies
     // add padding around player and enemy location to fit images
     // if those locations intersect reset payer to starting point
     for(var enemy in allEnemies){
         if (
-            player.y + 131 >= allEnemies[enemy].y + 90
-            && player.x + 25 <= allEnemies[enemy].x + 88
-            && player.y + 73 <= allEnemies[enemy].y + 135
-            && player.x + 76 >= allEnemies[enemy].x + 11) {
+            player.y + 130 >= allEnemies[enemy].y + 95
+            && player.x + 25 <= allEnemies[enemy].x + 85
+            && player.y + 75 <= allEnemies[enemy].y + 135
+            && player.x + 75 >= allEnemies[enemy].x + 15) {
             player.reset();
+            deaths += 1;
         }
     }
+
+    //Update Score Value
+    document.getElementById('score').value = score;
+    //Update deaths Value
+    document.getElementById('deaths').value = deaths;
 };
 
 // reset player position to same as starting point
@@ -146,6 +149,7 @@ var player = new Player(200, 400);
 var allEnemies=[enemy,enemy2,enemy3];
 
 var score = 0;
+var deaths = 0;
 
 
 
