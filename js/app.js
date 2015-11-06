d79756b0caaa9909189df43164bfe88acc1de314
Sage Elliott <wildtroll@gmail.com>
2015-11-05 17:05:37 -0800

    Remove some of the Udacity Comments

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 "use strict";
 // Enemies our player must avoid
 var Enemy = function(x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -16,9 +14,6 @@ var Enemy = function(x,y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 
     // if enemy x location is less than canvas length:
         // add 200 to x value multiplied by supplied time delta(dt)
@@ -36,10 +31,6 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 // Player class which the user controls
 var Player=function(x,y){
@@ -136,10 +127,6 @@ Player.prototype.reset= function(){
     this.y = 400;
 };
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 // Declare 3 enemies with hardcoded x&y values *For now*
 var enemy = new Enemy(-30,220);
